314f4efade317cf4be80a87e5e460a0bafc9c037
Park ChangHyub <thwlckd@naver.com>
2023-07-26 00:50:42 +0900

    feat: 캔버스 배경-지우개 색상 동일하게 변경, 컬러 태그 하드코딩 -> 리스트화
    - 컬러 태크: 상수 컬러 리스트로 관리, div -> ul > li 시멘틱 태그로 변경
    - prettierr tab-width: 4 -> 2

--- src/components/pages/GamePage/Canvas.jsx
+++ src/components/pages/GamePage/Canvas.jsx
@@ -1,70 +1,71 @@
 import { useEffect, useRef, useState } from 'react';
-import './Canvas.scss';
 import Controller from './Controller';
+import './Canvas.scss';
 
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
 
 const Canvas = () => {
-    const canvasRef = useRef();
-    const [ctx, setCtx] = useState();
-    const [isPainting, setIsPainting] = useState(false);
+  const canvasRef = useRef();
+  const [ctx, setCtx] = useState();
+  const [isPainting, setIsPainting] = useState(false);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        canvas.width = CANVAS_WIDTH;
-        canvas.height = CANVAS_HEIGHT;
-        const ctx = canvas.getContext('2d');
-        ctx.lineCap = 'round';
-        ctx.save();
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        ctx.restore();
-        setCtx(ctx);
-    }, []);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    const ctx = canvas.getContext('2d');
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 5;
+    ctx.save();
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.restore();
+    setCtx(ctx);
+  }, []);
 
-    const handleMouseMove = (event) => {
-        const x = event.clientX - event.target.offsetLeft;
-        const y = event.clientY - event.target.offsetTop;
-        if (isPainting) {
-            ctx.lineTo(x, y);
-            ctx.stroke();
-            return;
-        }
-        ctx.moveTo(x, y);
-    };
+  const handleMouseMove = (event) => {
+    const x = event.clientX - event.target.offsetLeft;
+    const y = event.clientY - event.target.offsetTop;
+    if (isPainting) {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      return;
+    }
+    ctx.moveTo(x, y);
+  };
 
-    const handleMouseDown = () => {
-        setIsPainting(true);
-    };
+  const handleMouseDown = () => {
+    setIsPainting(true);
+  };
 
-    const handleMouseUpOrLeave = () => {
-        setIsPainting(false);
-        ctx.beginPath();
-    };
+  const handleMouseUpOrLeave = () => {
+    setIsPainting(false);
+    ctx.beginPath();
+  };
 
-    const handleDoubleClick = (event) => {
-        const x = event.clientX - event.target.offsetLeft;
-        const y = event.clientY - event.target.offsetTop;
-        ctx.save();
-        ctx.font = '20pt serif';
-        ctx.strokeText('😛', x, y);
-        ctx.restore();
-    };
+  const handleDoubleClick = (event) => {
+    const x = event.clientX - event.target.offsetLeft;
+    const y = event.clientY - event.target.offsetTop;
+    ctx.save();
+    ctx.font = '20pt serif';
+    ctx.strokeText('😛', x, y);
+    ctx.restore();
+  };
 
-    return (
-        <div className="canvas-container">
-            <canvas
-                ref={canvasRef}
-                onMouseMove={handleMouseMove}
-                onMouseDown={handleMouseDown}
-                onMouseUp={handleMouseUpOrLeave}
-                onMouseLeave={handleMouseUpOrLeave}
-                onDoubleClick={handleDoubleClick}
-            ></canvas>
-            <Controller ctx={ctx} canvasRef={canvasRef} />
-        </div>
-    );
+  return (
+    <div className="canvas-container">
+      <canvas
+        ref={canvasRef}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+        onDoubleClick={handleDoubleClick}
+      ></canvas>
+      <Controller ctx={ctx} canvasRef={canvasRef} />
+    </div>
+  );
 };
 
 export default Canvas;
