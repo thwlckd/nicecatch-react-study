2ca280d27340a227b68d8866e70abadc00c8253c
Park ChangHyub <thwlckd@naver.com>
2023-07-27 04:37:59 +0900

    Feat: client-server 간 메세지 송수신 작업
    - react-scroll-to-bottom: 채팅 컨테이너에 스크롤 라이브러리 이용

--- src/components/pages/GamePage/Chat.jsx
+++ src/components/pages/GamePage/Chat.jsx
@@ -1,11 +1,63 @@
+import { useEffect, useState } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import './Chat.scss';
+
 const Chat = ({ socket, username, room }) => {
-  console.log(socket);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageList((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  const sendMessage = async () => {
+    if (message != '') {
+      const messageData = {
+        room,
+        author: username,
+        message,
+        time:
+          new Date(Date.now()).getHours() +
+          ':' +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit('send_message', messageData);
+      setMessageList((prev) => [...prev, messageData]);
+      setMessage('');
+    }
+  };
+
   return (
-    <>
-      <h3>Join A Chat</h3>
-      <div>{username}</div>
-      <div>{room}</div>
-    </>
+    <div className="chat-container">
+      <h3>Nice Chat</h3>
+      <ScrollToBottom className="message-container">
+        {messageList.map((messageData) => {
+          return (
+            <div
+              // key={Date.now()}
+              id={username === messageData.author ? 'you' : 'other'}
+            >
+              <span className="message-content">{messageData.message}</span>
+              <div className="message-meta">
+                <span>{messageData.time}</span>
+                <span>{messageData.author}</span>
+              </div>
+            </div>
+          );
+        })}
+      </ScrollToBottom>
+      <input
+        type="text"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyDown={(event) => {
+          event.key === 'Enter' && sendMessage();
+        }}
+      />
+      <button onClick={sendMessage}>&#9658;</button>
+    </div>
   );
 };
 
