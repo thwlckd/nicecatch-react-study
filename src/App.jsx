16b19a6fec760723118e74661b6708ef81aa6b54
Park ChangHyub <thwlckd@naver.com>
2023-08-07 18:00:42 +0900

    Change: 라우팅 페이지 수정
    - homepage -> 로그인/회원가입 페이지로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/pages/HomePage/HomePage';
+import Login from './components/pages/LoginPage/LoginPage';
 import Layout from './components/atoms/Layout/Layout';
 import Game from './components/pages/GamePage/GamePage';
+import SignUp from './components/pages/SignUpPage/SignUpPage';
+import WaitingRoom from './components/pages/WaitingPage/WaitingPage';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/game" element={<Game />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/wating-room" element={<WaitingRoom />} />
+        <Route path="/game" element={<Game />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
